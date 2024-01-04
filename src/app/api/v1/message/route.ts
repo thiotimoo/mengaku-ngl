376e538a19c71d001a7b688a0c713073a10e1905
thiotimoo <timoothio@gmail.com>
2024-01-04 13:58:35 +0700

    inconsistent empty ui

--- src/app/api/v1/message/route.ts
+++ src/app/api/v1/message/route.ts
@@ -1,9 +1,8 @@
 import prisma from '@/libs/prisma'
-import { Prisma } from '@prisma/client';
 
 export async function POST(request: Request) {
-    const { ...PostCreateBody } = await request.json();
-    const data = {...PostCreateBody  };
+    const { ...body } = await request.json();
+    const data = {...body  };
     const newMessage = await prisma.message.create({ data });
     if (!newMessage) return Response.json({ status: 500, message: "FAILED" });
     return Response.json({status: 200, message: "SUCCESS", data: newMessage});
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 import prisma from "@/libs/prisma";
-import {getUserDetails, getUserSession } from "@/libs/auth.libs";
+import { getUserDetails, getUserSession } from "@/libs/auth.libs";
 import MessageItem from "../MessageItem";
 
 const MessageList = async () => {
     const currentUser = await getUserDetails();
-    console.log(currentUser)
+    console.log(currentUser);
     if (!currentUser) return;
     const messages = await prisma.message.findMany({
-      orderBy: [
-        {
-          createdAt: 'desc',
-        },
-      ],
+        orderBy: [
+            {
+                createdAt: "desc",
+            },
+        ],
         where: {
             userId: currentUser.id,
-        }
+        },
     });
-    return <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-2">
-      {
-        messages.length ?
-        messages.map((message, index) => {
-          return <MessageItem key={message.id} {...message} />;
-        }) :
-        <p className="text-center p-6 bg-neutral-500">Belum ada apa-apa disini...</p>
-      }
-    </div>;
+    return messages.length ? (
+        <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-2">
+            {messages.map((message, index) => {
+                return <MessageItem key={message.id} {...message} />;
+            })}
+        </div>
+    ) : (
+        <p className="text-center p-6">Belum ada apa-apa disini...</p>
+    );
 };
-
 export default MessageList;
